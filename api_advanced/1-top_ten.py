c2c465892adf734e5f56076f2025cd2b35c28df4
selenai9 <s.isimbi2@alustudent.com>
2025-07-22 08:19:37 +0000

    dsg

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ 1-top_ten.py """
+
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    """Prints the titles of the first 10 hot posts listed in a subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         print(None)
         return
@@ -18,7 +19,7 @@ def top_ten(subreddit):
     if not posts:
         print(None)
         return
-    
+
     for post in posts:
         print(post['data']['title'])
 
