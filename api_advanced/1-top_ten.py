faf0fea4823c37c3d9ec538c2209e0cff86d6cd0
selenai9 <s.isimbi2@alustudent.com>
2025-07-22 08:16:38 +0000

    dsvxc

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,12 +5,22 @@ import requests
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
+    
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
+
+    posts = response.json().get('data', {}).get('children', [])
+    if not posts:
+        print(None)
+        return
+    
     for post in posts:
         print(post['data']['title'])
+
+# Example usage
+# top_ten('python')
